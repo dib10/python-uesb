3fe873b8139650a12cbb749c8bac53839d21cd9e
dib10 <caiodib10@gmail.com>
2024-04-13 20:33:30 -0300

    ex5

---
+++ ex1.py
@@ -0,0 +1,7 @@
+# 1) Elaborar um programa Python para somar dois números. 
+
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print(f"A soma entre {num1} e {num2} é igual a {soma}.")
