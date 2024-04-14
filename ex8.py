5d07e856f3ab952f72bc29a15f8f3f6f9fa254ec
dib10 <caiodib10@gmail.com>
2024-04-14 19:45:18 -0300

    ex8 alterado pra entender fibonacci

--- ex8.py
+++ ex8.py
@@ -1,14 +1,11 @@
-#8) Elaborar um programa Python para gerar a sequência de Fibonacci.
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-    
-
-n = int(input("Digite o valor de n: "))
-for i in range(n):
-    print(fibonacci(i))
+n = int(input("Digite o número: "))
+print("Sequência de Fibonacci")
+fib1 = 0
+fib2 = 1
+print(fib1, end=' ')
+print(fib2, end=' ')
+for i in range(2, n):
+    fib = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib
+    print(fib, end=' ')
