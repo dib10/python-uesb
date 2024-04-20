0c62578bf7d89a1cb54d5ad22b29e2fadfcb3a78
dib10 <caiodib10@gmail.com>
2024-04-20 16:08:18 -0300

    organizacao da pasta

--- exercicios/exercicios/ex14.py
+++
@@ -1,1 +0,0 @@
-#14) Elaborar um programa Python para criptografar uma string utilizando a cifra de C�sar. 
--- exercicios/exercicios/ex9.py
+++
@@ -1,7 +0,0 @@
-#9) Elaborar um programa Python para imprimir a tabuada de um numero. 
-
-def tabuada(n):
-    for i in range(0,11): #isso pq range nao engloba o ultimo numero
-        print(f"{n} x {i} = {n*i}")
-n = int(input("Digite o número: "))
-tabuada(n)
